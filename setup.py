c9a9047005a86d0274d1b76b9ab0a8b37ecfada2
Benat <bmdaj13@gmail.com>
2025-01-14 10:44:40 +0100

    prepare to publish package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_package",                # Package name
+    version="0.1.0",                  # Version
+    author="<Beñat Martinez de Aguirre Jokisch",      # Author's name
+    author_email="bmdaj13@gmail.com", # Author's email
+    description="A package to solve topology optimization problems that target optica forcesl based on the Maxwell Stress Tensor formalism.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bmdaj/MST_TopOpt",  # Repository URL
+    packages=find_packages(),         # Automatically find sub-packages
+    install_requires=[],              # List dependencies here or in requirements.txt
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",          # Minimum Python version
+)
